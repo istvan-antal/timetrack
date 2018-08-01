4c3545e6f96263565d26703ae47df718e093a15b
István Antal <i@istvan.xyz>
2018-08-01 07:30:00 +0100

    Renamed:    src/configureStore.ts -> src/store.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import configureStore from './configureStore';
+import configureStore from './store';
 import App from './App';
 
 const store = configureStore();
